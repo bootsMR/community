3439a3d304e02604bf6401d888ea44e1c38a8ef9
lin泽峰 <aa1003770783@qq.com>
2020-09-20 10:56:26 +0800

    “第二次，完善登录功能”
    Signed-off-by: public <aa1003770783@qq.com>

---
+++ src/main/java/life/majiang/community/community/controller/AuthorizeConrotller.java
@@ -0,0 +1,48 @@
+package life.majiang.community.community.controller;
+
+import life.majiang.community.community.dto.AccessTokenDTO;
+import life.majiang.community.community.dto.GithubUser;
+import life.majiang.community.community.provider.GithubProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+public class AuthorizeConrotller {
+    @Autowired//把Spring写好的实例，加载到Spring上下文
+    private GithubProvider githubProvider;
+    @Value("${github.client.id}")
+    private String clientId;
+    @Value("${github.client.secret}")
+    private String clientSecret;
+    @Value("${github.redirect.uri}")
+    private String redirectUri;
+    @GetMapping("/callback")
+    public String callback(@RequestParam(name="code")String code,
+                           @RequestParam(name="state")String state,
+                           HttpServletRequest request){
+        AccessTokenDTO accessTokenDTO=new AccessTokenDTO();
+        accessTokenDTO.setCode(code);
+        accessTokenDTO.setClient_id(clientId);
+        accessTokenDTO.setClient_secret(clientSecret);
+        accessTokenDTO.setRedirect_uri(redirectUri);
+        accessTokenDTO.setState(state);
+        String accessToken = githubProvider.getAccessToken(accessTokenDTO);
+        GithubUser user = githubProvider.getUser(accessToken);
+        //System.out.println(user.getName());
+        if(user!=null){
+            request.getSession().setAttribute("user",user);
+            return "redirect:/";
+            //登录成功,写cookie 和session
+
+        }else {
+            //登录失败，请重新登录
+            return "redirect:/";
+        }
+
+    }
+}
---
+++ src/main/java/life/majiang/community/community/dto/GithubUser.java
@@ -0,0 +1,41 @@
+package life.majiang.community.community.dto;
+
+public class GithubUser {
+    private String name;
+    private long id;
+
+    @Override
+    public String toString() {
+        return "GithubUser{" +
+                "name='" + name + '\'' +
+                ", id=" + id +
+                ", bio='" + bio + '\'' +
+                '}';
+    }
+
+    private String bio;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getBio() {
+        return bio;
+    }
+
+    public void setBio(String bio) {
+        this.bio = bio;
+    }
+}
